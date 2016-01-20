52297bd2f7677a34cddc2976232b6ec48a0f5ef2
Jose Elera Campana <jelera@gmail.com>
2016-01-20 02:37:35 -0600

    Add header comment

--- install_scripts/install_ubuntu_trusty.sh
+++ install_scripts/install_ubuntu_trusty.sh
@@ -1,5 +1,40 @@
 #!/bin/bash
-
+###############################################################################
+#         Name: install_ubuntu_trusty
+#        Usage: sudo ./install_ubuntu_trusty.sh
+#
+#  Description: This is a helper script that automates the installation of
+#                software for Development, SysAdmin, etc.
+#
+# Last Updated: Wed 20 Jan 2016 02:36:46 AM CST
+#
+#    Tested on: Ubuntu 14.04 LTS Trusty Tahr
+#
+#   Maintainer: Jose Elera (https://github.com/jelera)
+#     Based on: https://github.com/thoughtbot/laptop
+#      License: MIT
+#               Copyright (c) 2016 Jose Elera Campana
+#               Permission is hereby granted, free of charge, to any person
+#               obtaining a copy of this software and associated documentation
+#               files (the "Software"), to deal in the Software without
+#               restriction, including without limitation the rights to use,
+#               copy, modify, merge, publish, distribute, sublicense, and/or
+#               sell copies of the Software, and to permit persons to whom the
+#               Software is furnished to do so, subject to the following
+#               conditions:
+#
+#               The above copyright notice and this permission notice shall be
+#               included in all copies or substantial portions of the Software.
+#
+#               THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#               EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+#               OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#               NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+#               HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#               WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#               FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#               OTHER DEALINGS IN THE SOFTWARE.
+###############################################################################
 
 #############################################################################//
 #
