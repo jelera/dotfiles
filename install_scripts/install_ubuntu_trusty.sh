f5c29d4d5eab3de4a8233e2d366335352f3118b5
Jose Elera Campana <jelera@gmail.com>
2016-01-20 02:34:29 -0600

    Create install_ubuntu_trusty.sh

---
+++ install_scripts/install_ubuntu_trusty.sh
@@ -0,0 +1,495 @@
+#!/bin/bash
+
+
+#############################################################################//
+#
+# => HELPER FUNCTIONS
+#
+#############################################################################//
+
+function color_echo(){
+# Usage  : color_echo "string" color
+# Credit : http://stackoverflow.com/a/23006365/428786
+    local exp=$1;
+    local color=$2;
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+	case $(echo $color | tr '[:upper:]' '[:lower:]') in
+	    black) color=0 ;;
+	    red) color=1 ;;
+	    green) color=2 ;;
+	    yellow) color=3 ;;
+	    blue) color=4 ;;
+	    magenta) color=5 ;;
+	    cyan) color=6 ;;
+	    white|*) color=7 ;; # white or invalid color
+	esac
+    fi
+
+    tput setaf $color;
+    printf "\n$exp\n"
+    tput sgr0;
+}
+
+function figlet_echo(){
+    local exp=$1;
+    local color=$2;
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+	case $(echo $color | tr '[:upper:]' '[:lower:]') in
+	    black) color=0 ;;
+	    red) color=1 ;;
+	    green) color=2 ;;
+	    yellow) color=3 ;;
+	    blue) color=4 ;;
+	    magenta) color=5 ;;
+	    cyan) color=6 ;;
+	    white|*) color=7 ;; # white or invalid color
+	esac
+    fi
+
+    tput setaf $color;
+    printf "\n$exp"
+    tput sgr0;
+}
+
+
+
+#############################################################################//
+#
+# => SETUP
+#
+#############################################################################//
+ 
+#-----------------------------//
+# => Exit install script if 
+#    any command fails
+#-----------------------------//
+set -e
+
+#-----------------------------//
+# => Require root privileges
+#-----------------------------//
+if [[ $EUID -ne 0 ]]; then
+    color_echo "----------------------------------------" red
+    color_echo "|  This script must be run with sudo.  |" red
+    color_echo "----------------------------------------" red
+    exit 1
+fi
+
+#-----------------------------//
+# => Distro Testing
+#-----------------------------//
+if ! grep -qiE 'trusty' /etc/os-release; then
+    color_echo "----------------------------------------------------" red
+    color_echo "|  Sorry! we don't currently support that distro.  |" red
+    color_echo "----------------------------------------------------" red
+    exit 1
+fi
+
+
+
+
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░▀█▀░█▀█░█▀▀░▀█▀░█▀█░█░░░█░░░░░█▀▀░█▀█░█▀▀░▀█▀░█░█░█▀█░█▀▄░█▀▀░░░" green
+figlet_echo "░░░░█░░█░█░▀▀█░░█░░█▀█░█░░░█░░░░░▀▀█░█░█░█▀▀░░█░░█▄█░█▀█░█▀▄░█▀▀░░░" green
+figlet_echo "░░░▀▀▀░▀░▀░▀▀▀░░▀░░▀░▀░▀▀▀░▀▀▀░░░▀▀▀░▀▀▀░▀░░░░▀░░▀░▀░▀░▀░▀░▀░▀▀▀░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░█░█░█▀▄░█░█░█▀█░▀█▀░█░█░░░█▀▄░█▀▀░█▀▀░█░█░▀█▀░█▀█░█▀█░░░░░░" green
+figlet_echo "░░░░░░░░█░█░█▀▄░█░█░█░█░░█░░█░█░░░█░█░█▀▀░▀▀█░█▀▄░░█░░█░█░█▀▀░░░░░░" green
+figlet_echo "░░░░░░░░▀▀▀░▀▀░░▀▀▀░▀░▀░░▀░░▀▀▀░░░▀▀░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n\n" green
+
+
+#############################################################################//
+#
+# => ADDING REPOSITORIES
+#
+#############################################################################//
+
+#-----------------------------//
+# => GetDeb and PlayDeb Repos
+#-----------------------------//
+color_echo "Adding GetDeb.net Repo" cyan
+  if grep -qiE 'trusty' /etc/os-release; then
+  	if ! grep -qiE 'Linux Mint' /etc/issue; then
+		apt-add-repository "deb http://archive.getdeb.net/ubuntu trusty-getdeb apps"
+		wget -q -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+		apt-get update
+	else
+		color_echo "Linux Mint already includes GetDeb Repo" yellow
+	fi
+  fi
+
+color_echo "Adding PlayDeb.net Repo" cyan
+  if grep -qiE 'trusty' /etc/os-release; then
+  	apt-add-repository "deb http://archive.getdeb.net/ubuntu trusty-getdeb games"
+  	wget -q -O- http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
+  	apt-get update
+  fi
+
+
+
+#############################################################################//
+#
+# => SOFTWARE INSTALLATION (DESKTOP)
+#
+#############################################################################//
+
+#===============================================//
+# => Look and Feel
+#===============================================//
+color_echo "Installing Fonts ... " cyan
+  apt-get install -y fonts-roboto
+  apt-get install -y fonts-inconsolata
+  apt-get install -y fonts-font-awesome
+
+color_echo "Install MS Core Fonts" cyan
+  echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
+  apt-get install -y ttf-mscorefonts-installer
+
+color_echo "Installing Themes and Icons ... " cyan
+  apt-get install -y shimmer-themes
+  apt-get install -y elementary-icon-theme
+  apt-get install -y humanity-icon-theme
+  apt-get install -y ubuntu-mono
+  apt-get install -y libreoffice-style-galaxy
+
+#===============================================//
+# => Desktop Software
+#===============================================//
+color_echo "Installing restricted extras ... " cyan
+  apt-get install -y ubuntu-restricted-extras
+
+color_echo "Installing Skype ... " cyan
+  apt-get install -y skype
+
+color_echo "Installing Dropbox ... " cyan
+  apt-get install -y dropbox
+
+color_echo "Installing Filezilla FTP Client ... " cyan
+  apt-get install -y filezilla
+
+color_echo "Installing Font Manager ... " cyan
+  apt-get install -y font-manager
+
+color_echo "Installing Hexchat IRC Client ... " cyan
+  apt-get install -y hexchat
+
+#===============================================//
+# => Multimedia
+#===============================================//
+color_echo "Installing VLC Media Player ... " cyan
+  apt-get install -y vlc
+
+color_echo "Installing Clementine Music Player ... " cyan
+  apt-get install -y clementine
+
+color_echo "Installing RadioTray, a lightweight Internet Radio Player ... " cyan
+  apt-get install -y radiotray
+
+#===============================================//
+# => Graphic Tools
+#===============================================//
+color_echo "Installing Inkscape Vector Image Editor ... " cyan
+  apt-get install -y inkscape
+
+color_echo "Installing Gimp Image Raster Editor ... " cyan
+  apt-get install -y gimp
+
+color_echo "Installing Gimp Plugins and extras ... " cyan
+  apt-get install -y gimp-plugin-registry gimp-data-extras gnome-xcf-thumbnailer gimp-gmic gimp-texturize
+
+color_echo "Installing Gthumb ... " cyan
+  apt-get install -y gthumb
+
+color_echo "Installing Gcolor2 Color Picker ... " cyan
+  apt-get install -y gcolor2
+
+color_echo "Installing Gpick Color Picker ... " cyan
+  apt-get install -y gpick
+
+color_echo "Installing Scribus, an Open Source Publishing Tool ... " cyan
+  apt-get install -y scribus
+
+#===============================================//
+# => Games
+#===============================================//
+color_echo "Installing Steam ... " cyan
+  apt-get install -y steam
+
+color_echo "Installing OpenTTD, a Open Source Transport Tycoon clone ... " cyan
+  apt-get install -y openttd
+
+color_echo "Installing DosBox,an Emulator of IBM PC compatible computer running DOS... " cyan
+  apt-get install -y dosbox
+
+
+#===============================================//
+# => Utilities
+#===============================================//
+color_echo "Installing Unzip ... " cyan
+  apt-get install -y unzip
+
+color_echo "Installing Curl ... " cyan
+  apt-get install -y curl
+
+color_echo "Installing Guake, the Dropdown Terminal ... " cyan
+  apt-get install -y guake
+
+color_echo "Installing LuckyBackup, a Rsync frontend ... " cyan
+  apt-get install -y guake
+
+#===============================================//
+# => PPA's, DEB's and Source Compilation
+#===============================================//
+#-----------------------------//
+# => Pithos
+#-----------------------------//
+color_echo "Installing Pithos, a Pandora.com client ... " cyan
+  add-apt-repository -y ppa:pithos/ppa
+  apt-get update
+  apt-get install -y pithos
+
+#-----------------------------//
+# => Pencil
+#-----------------------------//
+color_echo "Pencil, a wireframe and UI designer" cyan
+  cd /tmp
+  wget https://github.com/prikhi/pencil/releases/download/v2.0.15/pencil-2.0.15-ubuntu-all.deb
+  dpkg -i pencil-2.0.15-ubuntu-all.deb
+  sed -i -e 's/MaxVersion\=42/MaxVersion\=44/g' /usr/share/pencil/application.ini
+  cd $HOME
+
+#-----------------------------//
+# => X-Tile
+#-----------------------------//
+color_echo "Installing X-tile Tiling Manager ... " cyan
+  cd /tmp
+  wget http://www.giuspen.com/software/x-tile_2.5.1-1_all.deb
+  dpkg -i x-tile_2.5.1-1_all.deb
+  cd -
+
+#-----------------------------//
+# => Albert Launcher
+#-----------------------------//
+color_echo "Installing Albert Launcher ... " cyan
+  add-apt-repository -y ppa:flexiondotorg/albert
+  apt-get update
+  apt-get install -y albert
+
+#-----------------------------//
+# => Zeal Docs
+#-----------------------------//
+color_echo "Installing Zeal Documentation Browser ... " cyan
+  add-apt-repository -y ppa:zeal-developers/ppa
+  apt-get update
+  apt-get install -y zeal
+
+#-----------------------------//
+# => Shutter
+#-----------------------------//
+color_echo "Installing Shutter Screenshot Manager ... " cyan
+  add-apt-repository -y ppa:shutter/ppa
+  apt-get update
+  apt-get install -y shutter
+
+#-----------------------------//
+# => Wine
+#-----------------------------//
+color_echo "Installing Wine, Wine is not an Emulator ... " cyan
+  add-apt-repository -y ppa:ubuntu-wine/ppa
+  apt-get update
+  apt-get install -y wine1.7
+
+#-----------------------------//
+# => PlayOnLinux
+#-----------------------------//
+color_echo "Installing PlayOnLinux, A Frontend for Wine ... " cyan
+  wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
+  wget http://deb.playonlinux.com/playonlinux_trusty.list -O /etc/apt/sources.list.d/playonlinux.list
+  apt-get update
+  apt-get install -y playonlinux
+
+
+
+#############################################################################//
+#
+# => SOFTWARE INSTALLATION (DEVELOPMENT)
+#
+#############################################################################//
+
+#===============================================//
+# => Development Tools
+#===============================================//
+color_echo "Installing Vim text editor... " cyan
+  apt-get install -y vim-gtk
+
+color_echo "Installing ZSH shell ... " cyan
+  apt-get install -y zsh
+
+color_echo "Installing Tmux terminal multiplexer ... " cyan
+  apt-get install -y tmux
+
+color_echo "Installing Htop ... " cyan
+  apt-get install -y htop
+
+color_echo "Installing Meld Diff Viewer ... " cyan
+  apt-get install -y meld
+
+color_echo "Installing Giggle Git Viewer ... " cyan
+  apt-get install -y giggle
+
+
+color_echo "Installing Ag The Silver Searcher as a Grep replacement ... " cyan
+  apt-get install -y silversearcher-ag
+
+#===============================================//
+# => Common shared libraries for compilation
+#===============================================//
+color_echo "Installing Essential tools for building and compiling ... " cyan
+  apt-get install -y build-essential automake
+
+color_echo "Installing Ruby 2.0 Development Libraries ... " cyan
+  apt-get install -y ruby2.0-dev
+
+color_echo "Installing Python 3.4 Development Libraries ... " cyan
+  apt-get install -y python3.4-dev
+
+color_echo "Installing Common Libraries for Python and Ruby ... " cyan
+  apt-get install -y libxslt1-dev libcurl4-openssl-dev libksba8 libksba-dev libqtwebkit-dev libreadline-dev libssl-dev zlib1g-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev python-software-properties libffi-dev postgresql-server-dev-all python-pip libbz2-dev
+
+#===============================================//
+# => STACKS
+#===============================================//
+color_echo "Installing LAMP Stack ... " cyan
+  apt-get install -y lamp-server^
+
+
+#===============================================//
+# => PPA's, DEB's and Source Compilation
+#===============================================//
+
+#-----------------------------//
+# => Node.js
+#-----------------------------//
+color_echo "Installing Node.js ... " cyan
+  curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+  apt-get install -y nodejs
+
+#-----------------------------//
+# => Universal Ctags
+#-----------------------------//
+color_echo "Installing Universal Ctags, a more updated alternative to Exuberant Ctags ... " cyan
+  # Getting Ctags
+  wget -q https://github.com/universal-ctags/ctags/archive/master.zip -O /tmp/u_ctags.zip
+  unzip -q /tmp/u_ctags.zip; cd ctags-master
+
+  # Building Ctags
+  ./autogen.sh
+  ./configure --prefix=/usr/local
+  make
+  make install
+  cd $HOME
+
+#-----------------------------//
+# => Git SCM
+#-----------------------------//
+color_echo "Installing Git Source Code Management ... " cyan
+  add-apt-repository -y ppa:git-core/ppa
+  apt-get update
+  apt-get install -y git
+
+#-----------------------------//
+# => VirtualBox
+#-----------------------------//
+color_echo "Installing VirtualBox 5 ... " cyan
+  if grep -qiE 'trusty' /etc/os-release; then
+  	apt-add-repository "deb http://download.virtualbox.org/virtualbox/debian trusty contrib"
+	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+  	apt-get update
+  	apt-get install -y virtualbox-5.0
+  fi
+
+#-----------------------------//
+# => Vagrant
+#-----------------------------//
+color_echo "Installing Vagrant 1.8 ... " cyan
+  cd /tmp
+  wget https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb
+  dpkg -i vagrant_1.8.1_x86_64.deb
+  cd -
+
+#-----------------------------//
+# => Git-cola
+#-----------------------------//
+color_echo "Installing Git-Cola Git Viewer ... " cyan
+  apt-get install -y python3-pyqt4
+  cd /tmp
+  wget https://github.com/git-cola/git-cola/archive/v2.5.tar.gz -O git-cola.tar.gz
+  tar xfz git-cola.tar.gz
+  cd git-cola-2.5
+  python setup.py install
+  cd $HOME
+
+#===============================================//
+# => NPM Packages
+#===============================================//
+color_echo "Upgrading npm with npm" cyan
+  npm install -g npm
+
+color_echo "Installing JSHint, Unopinionated JavaScript Linter ... " cyan
+  npm install -g jshint
+
+color_echo "Installing Tern, Code-analysis engine for JavaScript ... " cyan
+  npm install -g tern
+
+color_echo "Installing JSCtags, Ctags tags generator for JavaScript Sources ... " cyan
+  npm install -g git+https://github.com/ramitos/jsctags.git
+
+color_echo "Installing Gulp Task Runner ... " cyan
+  npm install -g gulp
+
+color_echo "Installing Grunt Task Runner ... " cyan
+  npm install -g grunt
+
+color_echo "Installing Bower Package Manager ... " cyan
+  npm install -g bower
+
+color_echo "Installing BrowserSync, Live preview for Web Development ... " cyan
+  npm install -g browser-sync
+
+color_echo "Installing Node-Sass, Node implementation of libsass ... " cyan
+  npm install -g node-sass
+
+color_echo "Installing Yeoman and generator for webapps ... " cyan
+  npm install -g yo
+  npm install -g generator-web
+  npm install -g generator-gulp-webapp
+
+
+
+#############################################################################//
+#
+# => CLEANUP
+#
+#############################################################################//
+
+color_echo "Cleaning up!..." cyan
+  apt-get purge banshee
+  rm -rf /tmp/* 2> /dev/null
+echo
+echo
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░█░█░█▀█░█░█░█▀▄░░░█▀▀░█▀█░█▄█░█▀█░█░█░▀█▀░█▀▀░█▀▄░░░" green
+figlet_echo "░░░░█░░█░█░█░█░█▀▄░░░█░░░█░█░█░█░█▀▀░█░█░░█░░█▀▀░█▀▄░░░" green
+figlet_echo "░░░░▀░░▀▀▀░▀▀▀░▀░▀░░░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░░▀░░▀▀▀░▀░▀░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░╻┏━┓░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░┃┗━┓░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░╹┗━┛░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░█▀▄░█▀▀░█▀█░█▀▄░█░█░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░█▀▄░█▀▀░█▀█░█░█░░█░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░▀░▀░▀▀▀░▀░▀░▀▀░░░▀░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n\n" green
