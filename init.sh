ed6bd6b2a2bc3eb8b76729314f35c36fabe30c12
Jose Elera Campana <jelera@gmail.com>
2016-01-20 15:40:46 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@
 #  Description: Simple script to autocreate the symlinks for my dotfiles and
 #               copy some helper programs to /usr/local/bin
 #
-# Last Updated: Mon 03 Nov 2014 12:59:15 PM CST
+# Last Updated: Wed 20 Jan 2016 03:38:29 PM CST
 #
 #    Tested on: Ubuntu 14.04 LTS Trusty Tahr
 #               CentOS 7 / Red Hat Enterprise Linux 7
@@ -38,15 +38,36 @@
 #               FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 #               OTHER DEALINGS IN THE SOFTWARE.
 ###############################################################################
+#############################################################################//
+#
+# => HELPER FUNCTIONS
+#
+#############################################################################//
 
-##---------------------------------------------------------------------------//
-##
-## => HELPER FUNCTIONS
-##
-##---------------------------------------------------------------------------//
 function color_echo(){
-	# Usage  : color_echo "string" color
-	# Credit : http://stackoverflow.com/a/23006365/428786
+# Usage  : color_echo "string" color
+# Credit : http://stackoverflow.com/a/23006365/428786
+	local exp=$1;
+	local color=$2;
+	if ! [[ $color =~ '^[0-9]$' ]] ; then
+	case $(echo $color | tr '[:upper:]' '[:lower:]') in
+		black) color=0 ;;
+		red) color=1 ;;
+		green) color=2 ;;
+		yellow) color=3 ;;
+		blue) color=4 ;;
+		magenta) color=5 ;;
+		cyan) color=6 ;;
+	white|*) color=7 ;; # white or invalid color
+	esac
+	fi
+
+	tput setaf $color;
+	printf "\n$exp\n"
+	tput sgr0;
+}
+
+function figlet_echo(){
 	local exp=$1;
 	local color=$2;
 	if ! [[ $color =~ '^[0-9]$' ]] ; then
@@ -61,18 +82,28 @@ function color_echo(){
 			white|*) color=7 ;; # white or invalid color
 		esac
 	fi
+
 	tput setaf $color;
-	echo $exp
+	printf "\n$exp"
 	tput sgr0;
 }
 
 
-color_echo "#######" green
-color_echo "init.sh" green
-color_echo "#######" green
 echo
-color_echo "MAKE SURE, YOU CLONE THIS GIT REPO" red
-color_echo "INSIDE THE ~/.config DIRECTORY" red
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░▀█▀░█▀█░▀█▀░▀█▀░░░░█▀▀░█░█░░░" green
+figlet_echo "░░░░█░░█░█░░█░░░█░░░░░▀▀█░█▀█░░░" green
+figlet_echo "░░░▀▀▀░▀░▀░▀▀▀░░▀░░▀░░▀▀▀░▀░▀░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n\n" green
+echo
+color_echo "---------------------------------------" red
+color_echo "|                                     |" red
+color_echo "|  MAKE SURE YOU CLONE THIS GIT REPO  |" red
+color_echo "|   INSIDE THE ~/.config DIRECTORY    |" red
+color_echo "|                                     |" red
+color_echo "---------------------------------------" red
 sleep 2
 
 
@@ -82,48 +113,48 @@ sleep 2
 ##
 ##---------------------------------------------------------------------------//
 echo
-color_echo "Backup the old dotfiles (up to 2 times) ..." cyan
-if [ -d ~/.old.dotfiles ]; then
+color_echo "Backup the old dotfiles (up to 2 times) to ~/.dotfiles.old ..." cyan
+if [ -d ~/.dotfiles.old ]; then
 	if [ -d /tmp/old.dotfiles ]; then
 		rm -rf /tmp/old.dotfiles
 	fi
-	mv -f ~/.old.dotfiles /tmp/old.dotfiles
+	mv -f ~/.dotfiles.old /tmp/old.dotfiles
 fi
-mkdir -p ~/.old.dotfiles
+mkdir -p ~/.dotfiles.old
 
 if [[ -f ~/.profile || -L ~/.profile ]]; then
-	mv -f ~/.profile ~/.old.dotfiles
+	mv -f ~/.profile ~/.dotfiles.old
 fi
 if [[ -f ~/.bashrc || -L ~/.bashrc ]]; then
-	mv -f ~/.bashrc ~/.old.dotfiles
+	mv -f ~/.bashrc ~/.dotfiles.old
 fi
 if [[ -f ~/.zshrc || -L ~/.zshrc ]]; then
-	mv -f ~/.zshrc ~/.old.dotfiles
+	mv -f ~/.zshrc ~/.dotfiles.old
 fi
 if [[ -f ~/.gitconfig || -L ~/.gitconfig ]]; then
-	mv -f ~/.gitconfig ~/.old.dotfiles
+	mv -f ~/.gitconfig ~/.dotfiles.old
 fi
 if [[ -f ~/.gitignore_global || -L ~/.gitignore_global ]]; then
-	mv -f ~/.gitignore_global ~/.old.dotfiles
+	mv -f ~/.gitignore_global ~/.dotfiles.old
 fi
 if [[ -f ~/.tmux.conf || -L ~/.tmux.conf ]]; then
-	mv -f ~/.tmux.conf ~/.old.dotfiles
+	mv -f ~/.tmux.conf ~/.dotfiles.old
 fi
 if [[ -f ~/.vimrc || -L ~/.vimrc ]]; then
-	mv -f ~/.vimrc ~/.old.dotfiles
+	mv -f ~/.vimrc ~/.dotfiles.old
 fi
 if [[ -d ~/.vim || -L ~/.vim ]]; then
-	mv -f ~/.vim ~/.old.dotfiles
+	mv -f ~/.vim ~/.dotfiles.old
 fi
 if [[ -d ~/.bin || -L ~/.bin ]]; then
-	mv -f ~/.bin ~/.old.dotfiles
+	mv -f ~/.bin ~/.dotfiles.old
 fi
 if [[ -d ~/.oh-my-zsh || -L ~/.oh-my-zsh ]]; then
-	mv -f ~/.oh-my-zsh ~/.old.dotfiles
+	mv -f ~/.oh-my-zsh ~/.dotfiles.old
 fi
 
 echo
-color_echo "You can find them at ~/.old.dotfiles" cyan
+color_echo "You can find them at ~/.dotfiles.old" cyan
 sleep 1.5
 
 
@@ -145,16 +176,16 @@ sleep 1
 ##---------------------------------------------------------------------------//
 echo
 color_echo "Creating the Symlinks ..." cyan
-ln -s $HOME/.config/dotfiles/bin               $HOME/.bin
-ln -s $HOME/.config/dotfiles/bash/bashrc       $HOME/.bashrc
-ln -s $HOME/.config/dotfiles/bash/bash_profile $HOME/.profile
+ln -s $HOME/.config/dotfiles/bin                  $HOME/.bin
+ln -s $HOME/.config/dotfiles/bash/bashrc          $HOME/.bashrc
+ln -s $HOME/.config/dotfiles/bash/bash_profile    $HOME/.profile
 
 ln -s $HOME/.config/dotfiles/gitconfig/gitconfig  $HOME/.gitconfig
 ln -s $HOME/.config/dotfiles/gitconfig/gitignore  $HOME/.gitignore_global
 
-ln -s $HOME/.config/dotfiles/tmux/tmux.conf    $HOME/.tmux.conf
+ln -s $HOME/.config/dotfiles/tmux/tmux.conf       $HOME/.tmux.conf
 
-ln -s $HOME/.config/dotfiles/zsh/zshrc         $HOME/.zshrc
+ln -s $HOME/.config/dotfiles/zsh/zshrc            $HOME/.zshrc
 sleep 1
 
 
@@ -164,14 +195,21 @@ sleep 1
 ##
 ##---------------------------------------------------------------------------//
 echo
+color_echo "---------------------------------------" yellow
+color_echo "|                                      |" yellow
+color_echo "|  Setting up Vim Configuration Files  |" yellow
+color_echo "|                                      |" yellow
+color_echo "----------------------------------------" yellow
+echo
 color_echo "Checking if dotvim repo is installed..." cyan
-color_echo "If it is... it will be moved to .old.dotfiles dir" red
+echo
+color_echo "If it is... it will be moved to .dotfiles.old dir" red
 sleep 2
 if [[ -d ~/.config/dotfiles/vim || -L ~/.config/dotfiles/vim ]]; then
-	mv -f ~/.config/dotfiles/vim ~/.old.dotfiles
+	mv -f ~/.config/dotfiles/vim ~/.dotfiles.old
 fi
 
-color_echo "Setting up Vim ..." cyan
+color_echo "Cloning my dotvim repo ..." cyan
 # Cloning my dotvim repo and create the symlinks
 git clone git@github.com:jelera/vimconfig.git ~/.config/dotfiles/vim
 ln -s $HOME/.config/dotfiles/vim     $HOME/.vim
@@ -214,6 +252,10 @@ sudo cp $HOME/.config/dotfiles/bin/vcprompt /usr/local/bin/
 sleep 1
 
 echo
-color_echo "#####################################################" green
-color_echo "Finished making the symlinks and cloning my vim setup" green
-color_echo "#####################################################" green
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
+figlet_echo "░░░▀█▀░█░█░█▀▀░░░█▀▀░█▀█░█▀▄░░░"
+figlet_echo "░░░░█░░█▀█░█▀▀░░░█▀▀░█░█░█░█░░░"
+figlet_echo "░░░░▀░░▀░▀░▀▀▀░░░▀▀▀░▀░▀░▀▀░░░░"
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"
+figlet_echo "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n\n"
