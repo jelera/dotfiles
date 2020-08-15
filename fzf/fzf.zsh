5f430967a8f369c2171bed287ca3e29992e7edfd
Jose Elera <jelera@users.noreply.github.com>
2020-08-14 19:51:43 -0500

    Changes

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,16 +1,16 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/jelera/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/jelera/.fzf/bin"
+if [[ ! "$PATH" == *"$HOME"/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/jelera/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/jelera/.fzf/shell/key-bindings.zsh"
+source "$HOME/.fzf/shell/key-bindings.zsh"
 
 bindkey -r '^T'
 bindkey -r '^P'
